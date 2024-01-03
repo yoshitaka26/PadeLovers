ad51dd85415a18c6eb4c653f323b81616afbe1ec
yoshitaka <yoshitaka.tanaka.1989@gmail.com>
2024-01-03 23:33:52 +0900

    switch to SwiftUI base

---
+++ PadeLovers/Home/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  PadeLovers
+//
+//  Created by Yoshitaka Tanaka on 2024/01/03.
+//  Copyright © 2024 Yoshitaka. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ PadeLovers/PadeLoversApp.swift
@@ -0,0 +1,18 @@
+//
+//  PadeLoversApp.swift
+//  PadeLovers
+//
+//  Created by Yoshitaka Tanaka on 2024/01/03.
+//  Copyright © 2024 Yoshitaka. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct PadeLoversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
