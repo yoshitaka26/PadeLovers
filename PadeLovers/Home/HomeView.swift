655a3447ae0f7cfac1f349a08485f09a0ae0b362
yoshitaka <yoshitaka.tanaka.1989@gmail.com>
2024-01-05 10:20:07 +0900

    create menu view

--- PadeLovers/Home/HomeView.swift
+++ PadeLovers/Home/HomeView.swift
@@ -10,7 +10,60 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                HStack(spacing: 20) {
+                    MenuButton(
+                        image: "btn_game_setting",
+                        size: geometry.size.width/3) {}
+                    MenuButton(
+                        image: "btn_game_record",
+                        size: geometry.size.width/3) {}
+                }
+                .frame(maxWidth: .infinity)
+                Spacer()
+                HStack(spacing: 20) {
+                    MenuButton(
+                        image: "btn_game_start",
+                        size: geometry.size.width/3) {}
+                    MenuButton(
+                        image: "btn_randomNumber_table",
+                        size: geometry.size.width/3) {}
+                }
+                .frame(maxWidth: .infinity)
+                Spacer()
+                HStack(spacing: 20) {
+                    MenuButton(
+                        image: "btn_uses",
+                        size: geometry.size.width/3) {}
+                    MenuButton(
+                        image: "btn_mainSetting",
+                        size: geometry.size.width/3) {}
+                }
+                .frame(maxWidth: .infinity)
+                Spacer()
+            }
+            .padding(20)
+        }
+    }
+
+    private struct MenuButton: View {
+        var image: String
+        var size: CGFloat = 100
+        var action: () -> Void
+
+        var body: some View {
+            Button(action: {
+                action()
+            }, label: {
+                Image(image)
+                    .resizable()
+                    .frame(width: size, height: size)
+                    .foregroundColor(.black)
+            })
+            .padding(20)
+        }
     }
 }
 
